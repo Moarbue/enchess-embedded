b17f0e86f78e7a08854217b5d0e35d1f9d729c7d
Moarbue <83272310+Moarbue@users.noreply.github.com>
2023-04-15 21:31:47 +0200

    Change ble uuid and stepper config

--- main/config.h
+++ main/config.h
@@ -4,9 +4,9 @@
 #define ENCHESS_VERSION_MAJOR 1
 #define ENCHESS_VERSION_MINOR 0
 
-#define ENCHESS_SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
-#define ENCHESS_CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
-#define ENCHESS_CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+#define ENCHESS_SERVICE_UUID           "bec6075b-97fc-4f3b-a693-f98ace1b5913" // UART service UUID
+#define ENCHESS_CHARACTERISTIC_UUID_RX "fb3a5803-d9f9-46b7-b418-9423a7320c08"
+#define ENCHESS_CHARACTERISTIC_UUID_TX "ec344566-b4c4-48ca-ad36-a3317b42fbde"
 
 // hardware specific configuration
 #define ENCHESS_BOARD_SIZE           240 // board size in mm
@@ -17,8 +17,8 @@
 
 // general configuration
 #define ENCHESS_STEP_DELAY           300 // delay between steps in µs
-#define ENCHESS_MICROSTEPS           8   // divider for each step
-#define ENCHESS_STALLGUARD_THRS      100 // sensibility of stall detection 0...255
+#define ENCHESS_MICROSTEPS           256 // divider for each step
+#define ENCHESS_STALLGUARD_THRS      90 // sensibility of stall detection 0...255
 
 // homing related configuration
 #define ENCHESS_HOME_STEP_DELAY      300 // delay between steps in µs during homing
